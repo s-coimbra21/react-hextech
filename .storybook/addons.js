f89e7b8bd1644afb5bd80feb152532da05af1afe
Sergio Coimbra <sercoimbra@gmail.com>
2017-04-02 16:13:56 +0100

    name

--- .storybook/addons.js
+++
@@ -1,3 +0,0 @@
-import '@kadira/storybook/addons';
-
-import '@kadira/storybook-addon-options/register';
--- stories/Todo.js
+++
@@ -1,6 +0,0 @@
-import { storiesOf } from '@kadira/storybook';
-
-storiesOf('TODO', module)
-  .add('Mastery Builder', () => null)
-  .add('Rune Builder', () => null)
-  .add('Champion Squares w/ Mastery', () => null);
