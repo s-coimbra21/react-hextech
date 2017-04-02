daa6b58189be468121892130673301539cab2b44
Sergio Coimbra <sercoimbra@gmail.com>
2017-04-02 23:16:08 +0100

    Add a slider with step of 1 to storybook

--- stories/SliderInput.js
+++ stories/SliderInput.js
@@ -9,6 +9,9 @@ storiesOf('Slider Input', module)
   .add('normal', () => (
     <SliderInput hocClassName="slider-wrapper" onChange={debounce(action('slider-input-change'), 300)} onBlur={action('slider-input-blur')} />
   ))
+  .add('normal (step=1)', () => (
+    <SliderInput hocClassName="slider-wrapper" onChange={debounce(action('slider-input-change'), 300)} onBlur={action('slider-input-blur')} step={1} />
+  ))
   .add('with tooltip [TODO]', () => (
     <SliderInput hocClassName="slider-wrapper" onChange={debounce(action('slider-input-change'), 300)} onBlur={action('slider-input-blur')} tooltip />
   ))
