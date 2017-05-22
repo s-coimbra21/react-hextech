386ca22dc933a7640aed119bfcf7bedd47fb4745
Sergio Coimbra <sercoimbra@gmail.com>
2017-05-22 12:35:30 +0100

    Move files around

---
+++ src/components/RuneBuilder/RuneSlot/slots.js
@@ -0,0 +1,17 @@
+export default {
+  27: {
+    x: 83.8,
+    y: 125.41,
+    type: 'quintessence'
+  },
+  28: {
+    x: 444.7,
+    y: 125.41,
+    type: 'quintessence'
+  },
+  29: {
+    x: 265,
+    y: 434.5,
+    type: 'quintessence'
+  }
+};
---
+++ src/components/RuneBuilder/gradients.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default [
+  <linearGradient id="quintSelect" x1="0" y1="0" x2="0" y2="1">
+    <stop offset="50%" stopColor="#211F22" />
+    <stop offset="100%" stopColor="#502242" />
+  </linearGradient>,
+  <linearGradient id="quintSelectOuter" x1="0" y1="0" x2="0" y2="1">
+    <stop offset="50%" stopColor="#0E1116" />
+    <stop offset="100%" stopColor="#ab4e84" />
+  </linearGradient>
+];
